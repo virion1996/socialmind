eae17c848f280bec8070aa6c5388e4feb0dc8c05
Omar Krauss <virionkrauss@gmail.com>
2020-11-02 23:05:26 -0300

    cadastros arrumado 2

--- scripts/cadastro_paciente.js
+++ scripts/cadastro_paciente.js
@@ -335,7 +335,7 @@ function addUser(nomeValue, sobrenomeValue, loginValue, senhaValue, emailValue, 
 
     // Salva o novo banco de dados com o novo usuário no localStorage
     localStorage.setItem('db_user', JSON.stringify(db_user));
-    window.location.href = '../index.html';
+    window.location.href = 'https://virion1996.github.io/socialmind/';
 }
 
 // Inicializa as estruturas utilizadas pelo LoginApp
